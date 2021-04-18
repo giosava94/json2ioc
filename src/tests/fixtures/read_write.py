aa54f3446937d416ee39fb76de1c8327e4ff635f
Giovanni Savarese <savarese.giovanni94@gmail.com>
2021-04-18 11:14:34 +0200

    Merge functions and fixtures in read_write tests

--- src/tests/fixtures/read_write.py
+++ src/tests/fixtures/read_write.py
@@ -1,29 +1,16 @@
 import pytest, json
 from testfixtures import TempDirectory
 
+text_lines = ["First line\n", "Second line\n"]
+
 
 @pytest.fixture()
-def json_file():
+def json_text_dir():
     with TempDirectory() as dir:
         conf_data = {"example_name": {"example_property": "example_value"}}
         s = json.dumps(conf_data)
-        p = dir.write("example.json", s, "utf-8")
-        yield {"path": p, "data": conf_data}
-
-
-@pytest.fixture()
-def invalid_json_file():
-    with TempDirectory() as dir:
-        conf_data = {"example_name": {"example_property": "example_value"}}
-        s = json.dumps(conf_data)
-        p = dir.write("example.json", s[5:-2], "utf-8")
-        yield p
-
-
-@pytest.fixture()
-def text_file():
-    with TempDirectory() as dir:
-        lines = ["First line\n", "Second line\n"]
-        text = "".join(lines)
+        dir.write("correct.json", s, "utf-8")
+        dir.write("incorrect.json", s[5:-2], "utf-8")
+        text = "".join(text_lines)
         p = dir.write("text.txt", text, "utf-8")
-        yield {"path": p, "lines": lines, "text": text}
+        yield dir
--- src/tests/test_read_write.py
+++ src/tests/test_read_write.py
@@ -1,51 +1,45 @@
 import pytest, json
 import json2ioc.read_write as rw
-from fixtures.read_write import json_file, invalid_json_file, text_file
+from fixtures.read_write import json_text_dir, text_lines
 
 
-def test_load_data_from_json(json_file):
+def test_load_data_from_json(json_text_dir):
     """
-    `load_data_from_json` receives a valid path to a json file.
+    Test `load_data_from_json`.
     """
-    d = rw.load_data_from_json(json_file["path"])
-    assert d == json_file["data"]
 
+    # Receives a valid path to a json file.
+    d = rw.load_data_from_json(json_text_dir.getpath("correct.json"))
+    key = d.get("example_name")
+    assert not key is None
+    assert key.get("example_property") == "example_value"
 
-def test_load_data_from_json_invalid_path():
-    """
-    `load_data_from_json` receives an invalid path to a json file.
-    """
+    # Receives an invalid path to a json file.
     try:
         d = rw.load_data_from_json("invalid_path")
         assert 0
     except FileNotFoundError:
         assert 1
 
-
-def test_load_data_from_invlid_json(invalid_json_file):
-    """
-    `load_data_from_json` receives path to an invalid json file.
-    """
+    # Receives path to an invalid json file.
     try:
-        d = rw.load_data_from_json(invalid_json_file)
+        d = rw.load_data_from_json(json_text_dir.getpath("incorrect.json"))
         assert 0
     except json.JSONDecodeError:
         assert 1
 
 
-def test_load_lines_from_file(text_file):
+def test_load_lines_from_file(json_text_dir):
     """
-    `load_lines_from_file` receives a valid path to a file.
+    Test `load_lines_from_file`.
     """
-    d = rw.load_lines_from_file(text_file["path"])
+
+    # Receives a valid path to a file.
+    d = rw.load_lines_from_file(json_text_dir.getpath("text.txt"))
     for i in range(len(d)):
-        assert d[i] == text_file["lines"][i]
+        assert d[i] == text_lines[i]
 
-
-def test_load_lines_from_file_invalid_path():
-    """
-    `load_lines_from_file` receives an invalid path to a file.
-    """
+    # Receives an invalid path to a file.
     try:
         d = rw.load_lines_from_file("invalid_path")
         assert 0
@@ -53,18 +47,16 @@ def test_load_lines_from_file_invalid_path():
         assert 1
 
 
-def test_load_text_from_file(text_file):
+def test_load_text_from_file(json_text_dir):
     """
-    `load_text_from_file` receives a path to a file.
+    Test `load_text_from_file`
     """
-    d = rw.load_text_from_file(text_file["path"])
-    assert d == text_file["text"]
 
+    # Receives a path to a file.
+    d = rw.load_text_from_file(json_text_dir.getpath("text.txt"))
+    assert d == "".join(text_lines)
 
-def test_load_text_from_file_invalid_path():
-    """
-    `load_text_from_file` receives an invalid path to a file.
-    """
+    # Receives an invalid path to a file.
     try:
         d = rw.load_text_from_file("invalid_path")
         assert 0
@@ -72,94 +64,75 @@ def test_load_text_from_file_invalid_path():
         assert 1
 
 
-def test_write_lines_to_file(text_file):
+def test_write_lines_to_file(json_text_dir):
     """
-    `write_lines_to_file` receives a path to a file and a list.
+    Test `write_lines_to_file`.
     """
-    rw.write_lines_to_file(text_file["path"], text_file["lines"])
+
+    # Receives a path to a file and a list.
+    valid_path = json_text_dir.getpath("text.txt")
+    invalid_path = "invalid_path/file.txt"
+    rw.write_lines_to_file(valid_path, text_lines)
     assert 1
 
-
-def test_write_lines_to_invalid_file(text_file):
-    """
-    `write_lines_to_file` receives an invalid path to a file and a list.
-    """
+    # Receives an invalid path to a file and a list.
     try:
-        rw.write_lines_to_file("invalid/file.txt", text_file["lines"])
+        rw.write_lines_to_file(invalid_path, text_lines)
+        assert 0
+    except FileNotFoundError:
+        assert 1
+
+    # Receives a path to a file and try to write values that ar not lists.
+    rw.write_lines_to_file(valid_path, "string")
+    assert 1
+    try:
+        rw.write_lines_to_file(valid_path, 2)
+        assert 0
+    except TypeError:
+        assert 1
+
+    # Receives an invalid path to a file and invalid argument as list.
+    try:
+        rw.write_lines_to_file(invalid_path, 2)
         assert 0
     except FileNotFoundError:
         assert 1
 
 
-def test_write_lines_to_file_not_list_params(text_file):
+def test_write_text_to_file(json_text_dir):
     """
-    `write_lines_to_file` receives a path to a file
-    and try to write values that ar not lists.
+    Test `write_text_to_file`.
     """
-    rw.write_lines_to_file(text_file["path"], "string")
+
+    # Receives a path to a file and a text
+    valid_path = json_text_dir.getpath("text.txt")
+    invalid_path = "invalid_path/file.txt"
+    text = "".join(text_lines)
+    rw.write_text_to_file(valid_path, text)
     assert 1
-    try:
-        rw.write_lines_to_file(text_file["path"], 2)
-        assert 0
-    except TypeError:
-        assert 1
 
-
-def test_write_invalid_lines_to_invalid_file():
-    """
-    `write_lines_to_file` receives an invalid path to a file
-    and invalid argument as list.
-    """
+    # Receives an invalid path to a file and a text.
     try:
-        rw.write_lines_to_file("invalid/file.txt", 2)
+        rw.write_text_to_file(invalid_path, text)
         assert 0
     except FileNotFoundError:
         assert 1
 
-
-def test_write_text_to_file(text_file):
-    """
-    `write_text_to_file` receives a path to a file and a text.
-    """
-    rw.write_text_to_file(text_file["path"], text_file["text"])
-    assert 1
-
-
-def test_write_text_to_invalid_file(text_file):
-    """
-    `write_text_to_file` receives an invalid path to a file and a text.
-    """
+    # Receives a path to a file and try to write values that ar not strings.
     try:
-        rw.write_text_to_file("invalid/file.txt", text_file["text"])
-        assert 0
-    except FileNotFoundError:
-        assert 1
-
-
-def test_write_text_to_file_not_list_params(text_file):
-    """
-    `write_text_to_file` receives a path to a file
-    and try to write values that ar not strings.
-    """
-    try:
-        rw.write_text_to_file(text_file["path"], ["string"])
+        rw.write_text_to_file(valid_path, ["string"])
         assert 0
     except TypeError:
         assert 1
     try:
-        rw.write_text_to_file(text_file["path"], 2)
+        rw.write_text_to_file(valid_path, 2)
         assert 0
     except TypeError:
         assert 1
 
-
-def test_write_invalid_text_to_invalid_file():
-    """
-    `write_text_to_file` receives an invalid path to a file
-    and invalid argument as text.
-    """
+    # Receives an invalid path to a file and invalid argument as text.
     try:
-        rw.write_text_to_file("invalid/file.txt", 2)
+        rw.write_text_to_file(invalid_path, 2)
         assert 0
     except FileNotFoundError:
         assert 1
